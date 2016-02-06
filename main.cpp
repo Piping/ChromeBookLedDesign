ca29a3d6e0af71c4a149798272f1981b60a27eee
Piping <admin@ucdavis.me>
2016-02-05 22:36:09 -0800

    Initial LED Encoding Scheme
    Sync--100ms(On)-100ms(Off)-100ms(On)-Sync

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include "mbed.h"
+
+#define ON 1;
+#define OFF 0; 
+
+void blink(DigitalOut& led,unsigned int timeInterval,bool one);
+void blink_char(DigitalOut& led,unsigned int timeInterval,char c);
+void blink_str(DigitalOut& led,unsigned int timeInterval,char* str);
+void sync(DigitalOut& led,unsigned int timeInterval);
+
+//define bit from right to letf(most significant)
+const char BIT[8] = { 0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+
+char* testString = "12345678ABCDEGHI";
+DigitalOut led(LED1);
+
+void blink(DigitalOut& led,unsigned int timeInterval,bool one)
+{
+    if(one)
+    {   
+        led = ON;
+        wait_ms(timeInterval);
+        led = OFF;
+    }
+    else
+    {
+        wait_ms(timeInterval);
+    }
+}
+void blink_char(DigitalOut& led,unsigned int timeInterval,char c)
+{
+    for(int i = 0; i<8 ; i++){
+        blink(led,timeInterval,c & BIT[i]);
+    }
+}
+
+void sync(DigitalOut& led,unsigned int timeInterval)
+{
+    led = ON;
+    wait_ms(timeInterval);
+    led = OFF;
+}
+
+void blink_str(DigitalOut& led,unsigned int timeInterval,char* str)
+{
+    for(int i=0; str[i]!= '\0'; i++)
+    {
+        blink_char(led,timeInterval,str[i]);
+    }
+}
+int main(void) {
+    while(true)
+    {
+        sync(led,1000);
+        blink_str(led,100,testString);
+        sync(led,1000);
+    }
+    return 0;
+}
